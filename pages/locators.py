6daaf7fc6454f32183179dab4d87d467e5cc0991
Danil <52774117+CaptainLarsenBlood@users.noreply.github.com>
2022-02-20 14:13:53 +0500

    Add files via upload

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,19 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators():
+
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    TEXT_ADD_BASKET = (By.CSS_SELECTOR, ".alert-success .alertinner strong")
+    TITLE_PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
+    PRICE_PRODUCT_MESSAGE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, ".product_main .price_color")
+
